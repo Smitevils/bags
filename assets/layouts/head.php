2cefa715f125b3fcb8509fecc9086d80bf34bfd2
smitevils <smitevils@yandex.ru>
2015-08-11 17:09:27 +0300

    add fonts, animate.css, my styles

---
+++ assets/layouts/head.php
@@ -0,0 +1,10 @@
+<meta charset="utf-8">
+<title>Bags</title>
+<!-- my styles -->
+<link rel="stylesheet" href="assets/styles/master.css">
+<!-- my fonts -->
+<link rel="stylesheet" href="assets/styles/fonts.css">
+<!-- normalize.css -->
+<link rel="stylesheet" href="assets/styles/normalize.css">
+<!-- animate.css -->
+<link rel="stylesheet" href="assets/styles/animate.css">
---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<?php include_once "assets/layouts/head.php" ?>
+</head>
+<body>
+	<?php include_once "assets/layouts/main.php" ?>
+	<?php include_once "assets/layouts/scripts.php" ?>
+</body>
+</html>
