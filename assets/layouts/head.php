e13f05347f16eb496a08d0b08b77b3e8fa10933f
smitevils <smitevils@yandex.ru>
2015-08-14 09:49:49 +0300

    repeat coding

--- assets/layouts/head.php
+++ assets/layouts/head.php
@@ -8,3 +8,7 @@
 <link rel="stylesheet" href="assets/styles/normalize.css">
 <!-- animate.css -->
 <link rel="stylesheet" href="assets/styles/animate.css">
+<!--[if lt IE 9]>
+	<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+	<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+<![endif]-->
--- assets/layouts/main.php
+++ assets/layouts/main.php
@@ -3,15 +3,159 @@
 		<div class="head-menu">
 			<ul>
 				<li>
-					<a href="#">Подарки</a>
+					<a href="#"><i class="gift"></i>Подарки</a>
 				</li>
 				<li>
-					<a href="#">Wish лист</a>
+					<a href="#"><i class="heart"></i>Wish лист</a>
 					</li>
 				<li>
-					<a href="#">Личный кабинет</a>
+					<a href="#"><i class="key"></i>Личный кабинет</a>
 				</li>
 			</ul>
 		</div>
 	</div>
 </div>
+<header>
+	<!-- 
+	<div class="column clearfix head">
+		<div class="logo">
+			<a href="#">
+				<img src="assets/img/logo.png" alt="">
+			</a>
+		</div>
+		<div class="number">
+			<a href="#">+7 (812) 981-60-14</a>
+			<p>с 9.00 до 21.00 без выходных</p>
+		</div>
+	</div>
+	 -->
+	<div class="column clearfix head">
+		<table>
+			<tr>
+				<td class="logo">
+					<a href="#">
+						<img src="assets/img/logo.png" alt="лого">
+					</a>
+				</td>
+				<td class="number">
+					<a href="#">+7 (812) 981-60-14</a>
+					<p>с 9.00 до 21.00 без выходных</p>
+				</td>
+				<!-- <td class="call">
+					<a href="#" class="btn btn-1">ХОЧУ ОБРАТНЫЙ ЗВОНОК</a>
+				</td> -->
+				<td class="bid">
+					<a href="#" class="btn btn-1">ХОЧУ ОБРАТНЫЙ ЗВОНОК</a>
+					<a href="#" class="btn btn-2"><i class="basket"></i>2 товара 864р.</a>
+				</td>
+			</tr>
+		</table>
+	</div>
+	<div class="column clearfix">
+		<nav>
+			<ul>
+				<li>
+					<a href="#">Все сумки</a>
+				</li>
+				<li>
+					<a href="#">В наличии</a>
+				</li>
+				<li>
+					<a href="#">Почему мы</a>
+				</li>
+				<li>
+					<a href="#">Доставка и оплата</a>
+				</li>
+				<li>
+					<a href="#">Контакты</a>
+				</li>
+			</ul>
+		</nav>
+		<input type="text" class="search">
+	</div>
+</header>
+<main>
+	<div class="column">
+		<div class="big-content-block clearfix">
+			<div class="block big-block-left">
+				<div class="img-area">
+					<img src="assets/img/goods/bag-3.png" alt="сумка">
+				</div>
+				<div class="text">
+					<div class="title">
+						РАСПРОДАЖА
+					</div>
+					<h2>Сумка дня</h2>
+					<p>
+						Michael Kors Selma
+					</p>
+					<div class="price-old">7990 руб</div>
+					<div class="price-new">5450 руб</div>
+				</div>
+				<a href="#" class="btn btn-3">Смотреть</a>
+			</div>
+			<div class="block medium-block-right">
+				<div class="title">
+					Брендовые сумки 
+				</div>
+				<p>
+					Кожаные сумки по лучшим ценам от 2990 до 6490р.
+				</p>
+				<div class="img-area">
+					<table>
+						<tr>
+							<td>
+								<img src="assets/img/brand/brand-1.png" alt="бренд">
+							</td>
+							<td>
+								<img src="assets/img/brand/brand-2.png" alt="бренд">
+							</td>
+							<td>
+								<img src="assets/img/brand/brand-3.png" alt="бренд">
+							</td>
+						</tr>
+					</table>
+				</div>
+				<a href="#" class="btn btn-3">Смотреть</a>
+			</div>
+			<div class="block small-block-right">
+				<div class="title">
+					БЕСПЛАТНАЯ ДОСТАВКА И ПОДАРКИ
+				</div>
+				<p>
+					При каждой покупке вне зависимости от суммы чека
+				</p>
+				<a href="#" class="btn btn-4">Подробнее</a>
+			</div>
+			<div class="block big-second-block-left">
+				<div class="title">
+					кожаные сумки
+				</div>
+				<p>
+					Кожаные сумки по лучшим ценам от 2990 до 6490р.
+				</p>
+				<div class="img-area">
+					<img src="assets/img/goods/bag-2.png" alt="сумка">
+				</div>
+				<a href="#" class="btn btn-3">Смотреть</a>
+			</div>
+			<div class="block big-second-block-right">
+				<div class="title">
+					Сумки в наличии
+				</div>
+				<div class="img-area">
+					<img src="assets/img/goods/bag-1.png" alt="сумка">
+				</div>
+				<a href="#" class="btn btn-3">Смотреть</a>
+			</div>
+		</div>
+	</div>
+	<!-- section -->
+	<div class="column">
+		<div class="section">
+			<div class="title">
+				Новинки
+			</div>
+		</div>
+	</div>
+</main>
